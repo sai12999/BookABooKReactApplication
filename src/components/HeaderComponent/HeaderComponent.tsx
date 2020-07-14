406356fc48891c4012b4cae5cea934658c3e0996
kommi venkata sai samanth <saisamanth.kommi@refinitiv.com>
2020-07-14 21:45:30 +0530

    hh

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import * as classes from './HeaderComponent.css';
 
 const headerComponent = () => {
     return (
-        <header>Im the header</header>
+        <header className="pageheader">Im the header</header>
     )
 }
 
